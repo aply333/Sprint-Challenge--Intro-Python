bcc6cf5174653401c91a74a3770d677e7f223952
Andrei Lysenko <aply@localhost.localdomain>
2020-07-18 10:54:01 -0700

    MVP & Stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,12 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -16,19 +23,30 @@
 # should not be loaded into a City object.
 cities = []
 
-def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+
+
+
+def cityreader(cities=[]):
+  path = "src/cityreader/cities.csv"
+  csv_file  = open(path, newline="")
+  reader = csv.reader(csv_file)
+  header = next(reader)
+  for row in reader:
+    name = row[0]
+    lat = float(row[3])
+    lon = float(row[4])
+    cities.append(City(name, lat, lon))
+  return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(f"{c.name}, {c.lat}, {c.lon}")
 
 # STRETCH GOAL!
 #
@@ -63,9 +81,11 @@ for c in cities:
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
+  within = [f"{c.name}:({c.lat},{c.lon})" for c in cities if lat1 > c.lat > lat2 and lon1 > c.lon > lon2]
   
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
 
   return within
+
+print(cityreader_stretch(44,-101, 31, -121, cities))
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    pass
+
+class GroundVehicle(Vehicle):
+    """Parent is Vehicle"""
+    pass
+
+class Car(GroundVehicle):
+    """Parent is GroundVehicle"""
+    pass
+
+class Motorcycle(GroundVehicle):
+    """Parent is GroundVehicle"""
+    pass
+
+class FlightVehicle(Vehicle):
+    """Parent is Vehicle"""
+    pass
+
+class Airplane(FlightVehicle):
+    """ Parent is FlightVehicle"""
+    pass
+
+class Starship(FlightVehicle):
+    """Parent is Vehicle"""
+    pass
+    
